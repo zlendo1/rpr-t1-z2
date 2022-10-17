13a61745e702b6b34a708576aae9de471c324eea
zlendo1 <zlendo1@etf.unsa.ba>
2022-10-17 18:48:58 +0200

    Ispravka

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,12 +1,28 @@
+package ba.unsa.etf.rpr;
+
 import java.util.Scanner;
 
 public class Main {
+    public static int sumaCifara(int n) {
+        int suma = 0;
+
+        while (n > 0) {
+            suma = n % 10;
+            n = n / 10;
+        }
+
+        return suma;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Unesite dva broja: ");
-        int x = scanner.nextInt(), y = scanner.nextInt();
+        System.out.print("Unesite broj: ");
+        int n = scanner.nextInt();
 
-        System.out.println(x + " " + y);
+        for (int i = 1; i <= n; i++) {
+            if (i % sumaCifara(i) == 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
