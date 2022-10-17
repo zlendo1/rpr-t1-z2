c75045565981ea7cb91df73e5174b1712d6f2c9a
zlendo1 <zlendo1@etf.unsa.ba>
2022-10-17 14:40:31 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Unesite dva broja: ");
+        int x = scanner.nextInt(), y = scanner.nextInt();
+
+        System.out.println(x + " " + y);
+    }
+}
